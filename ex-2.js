f4aaadfffdf03f50366cca271ecbdac6f5b63006
AnthonyFS <anthonyfungesmith@gmail.com>
2026-06-09 14:08:58 +0700

    completed assignment 2

--- ex-2.js
+++ ex-2.js
@@ -5,16 +5,28 @@ const studentScoresRoom3 = [67, 80, 100, 15, 12, 40, 67, 80, 100, 67];
 
 function checkGradeOver70(score) {
   // Start coding here
+  return score > 70
 }
 
 function atLeastFive(array, operation) {
   // Start coding here
+  let count = 0;
+  for (let num of array){
+    if (operation(num)){
+      count += 1
+    }
+  }
+  if (count >= 5){
+    return true
+  } else {
+    return false
+  }
 }
 
 // Using `atLeastFive` function here
-let scoreRoom1Result;
-let scoreRoom2Result;
-let scoreRoom3Result;
+let scoreRoom1Result = atLeastFive(studentScoresRoom1,checkGradeOver70);
+let scoreRoom2Result = atLeastFive(studentScoresRoom2,checkGradeOver70);
+let scoreRoom3Result = atLeastFive(studentScoresRoom3,checkGradeOver70);
 
 console.log(scoreRoom1Result); //true
 console.log(scoreRoom2Result); //false
